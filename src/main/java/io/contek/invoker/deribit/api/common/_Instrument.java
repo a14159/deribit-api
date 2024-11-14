fcb2ccd2ec8b85a118fcd32d1df35eaa729043f7
kong <kong_k@hotmail.com>
2024-11-14 23:40:00 +0200

    update _Instrument fields (added instrument_id)

--- src/main/java/io/contek/invoker/deribit/api/common/_Instrument.java
+++ src/main/java/io/contek/invoker/deribit/api/common/_Instrument.java
@@ -7,20 +7,21 @@ import java.math.BigDecimal;
 public class _Instrument {
 
   public String base_currency;
+  public String quote_currency;
+  public String instrument_name;
+  public int instrument_id;
   public Double block_trade_commission;
   public BigDecimal contract_size;
   public Long creation_timestamp;
   public Long expiration_timestamp;
-  public String instrument_name;
   public Boolean is_active;
   public String kind;
   public Long leverage;
   public Double maker_commission;
+  public Double taker_commission;
   public BigDecimal min_trade_amount;
   public String option_type;
-  public String quote_currency;
   public String settlement_period;
   public Double strike;
-  public Double taker_commission;
   public BigDecimal tick_size;
 }
